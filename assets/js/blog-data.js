11a7618885f25d6426c452addaf8fc31995ba63f
Lordsfavour Anukam <leefav3@gmail.com>
2025-11-28 18:48:46 -0400

    Fetch blog posts from JSON file and render

--- assets/js/blog-data.js
+++ assets/js/blog-data.js
@@ -1,1 +1,9 @@
-
+// Load posts from /data/posts.json
+fetch("data/posts.json")
+  .then(res => res.json())
+  .then(posts => {
+    window.blogPosts = posts;   // make available globally
+    if (typeof renderBlogList === "function") renderBlogList(posts);
+    if (typeof renderSinglePost === "function") renderSinglePost(posts);
+  })
+  .catch(err => console.error("Error loading posts:", err));
